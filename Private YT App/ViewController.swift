c12cc9ca37cf1c5440b32b7d8c98ba864a7c720e
aljonleynes11 <aljonleynes11@gmail.com>
2020-06-18 17:54:24 +0800

    modified codes
    modified codes

--- Private YT App/ViewController.swift
+++ Private YT App/ViewController.swift
@@ -9,54 +9,54 @@
 import UIKit
 import YoutubePlayer_in_WKWebView
 class ViewController: UIViewController {
-
-   
-    @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var textField: UITextField!
     var passUrl = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-       
     }
 
     @IBAction func goNextBtn(_ sender: UIButton) {
         self.passUrl=textField.text!
-        print("Go next")
-       
         
         let ytUrl = textField.text!
         if ytUrl.contains("www.youtube.com") && ytUrl.count > 17
         {
             let ytUrl2 = textField.text?.replacingOccurrences(of: "https://www.youtube.com/watch?v=", with: "")
-            self.passUrl = ytUrl2!
-            textField.text = ""
-            self.performSegue(withIdentifier: "SecondViewSegue", sender: self)
-            
+             self.passUrl = ytUrl2!
+            goNext()
         }
         else if ytUrl.contains("https://youtu.be/") && ytUrl.count > 17
         {
             let ytUrl2 = textField.text?.replacingOccurrences(of: "https://youtu.be/", with: "")
-            self.passUrl = ytUrl2!
-            textField.text = ""
-            self.performSegue(withIdentifier: "SecondViewSegue", sender: self)
-            
+             self.passUrl = ytUrl2!
+            goNext()
         }
         else if ytUrl.contains("www.youtube.com") == false || ytUrl.contains("https://youtu.be/") == false || ytUrl.count <= 17
         {
-            print("Please enter valid youtube url -- to be fixed, show alert if invalid url")
-            //self.present(alert, animated: true, completion: nil)
-            
+            showAlert()
         }
         else
         {
-            print("Text box is nil")
+            showAlert()
         }
     }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let fromViewController = segue.destination as! SecondScreenViewController
         fromViewController.getUrl=self.passUrl
-   } 
+   }
+    
+    func showAlert(){
+        let alert = UIAlertController(title: "Invalid YouTube URL", message: "Please enter a valid YouTube URL", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        textField.text=""
+    }
+    func goNext(){
+       
+        textField.text = ""
+        self.performSegue(withIdentifier: "SecondViewSegue", sender: self)
+    }
+     
 }
